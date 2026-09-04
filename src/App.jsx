184271361b6e8213b59bd6199a0fbab2978b3f1a
naristo <52517964+naristo@users.noreply.github.com>
2026-09-04 17:41:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,20 @@ import {
 } from 'lucide-react';
 
 // --- SETUP FIREBASE ---
-const firebaseConfig = typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : {};
+// --- 1. SETUP FIREBASE ---
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const appId = typeof __app_id !== 'undefined' ? __app_id : 'demo-uang-kas-multikelas';
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+// const appId = 'kas-kelas-app';
 
 const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Ags', 'Sep', 'Okt', 'Nov', 'Des'];
 const CURRENT_YEAR = new Date().getFullYear();
